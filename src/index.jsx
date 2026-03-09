e460bb3b22124378466b593e66cfbe650a544a87
anastasiafoth <anastasiafoth9@gmail.com>
2026-03-09 10:32:32 +0100

    Added styling to Header, Footer and Home()

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import "./index.css";
 
 function App() {
   return (
@@ -23,15 +24,17 @@ function App() {
 function Home() {
   return (
     <>
-      <h1>You got the travel plans, we got the travel vans.</h1>
-      <p>
-        Add adventure to your life by joining the #vanlife movement. Rent the
-        perfect van to make your perfect road trip.
-      </p>
+      <div className="main-content">
+        <h1>You got the travel plans, we got the travel vans.</h1>
+        <p>
+          Add adventure to your life by joining the #vanlife movement. Rent the
+          perfect van to make your perfect road trip.
+        </p>
 
-      <button className="btn-orange">Find your van</button>
+        <button className="btn-orange">Find your van</button>
+      </div>
 
-      <footer>Ⓒ 2022 #VANLIFE</footer>
+      <footer>Ⓒ 2026 #VANLIFE</footer>
     </>
   );
 }
@@ -39,18 +42,20 @@ function Home() {
 function About() {
   return (
     <>
-      <h1>Don’t squeeze in a sedan when you could relax in a van.</h1>
-      <p>
-        Our mission is to enliven your road trip with the perfect travel van
-        rental. Our vans are recertified before each trip to ensure your travel
-        plans can go off without a hitch. (Hitch costs extra 😉) Our team is
-        full of vanlife enthusiasts who know firsthand the magic of touring the
-        world on 4 wheels.
-      </p>
+      <div className="main-content">
+        <h1>Don’t squeeze in a sedan when you could relax in a van.</h1>
+        <p>
+          Our mission is to enliven your road trip with the perfect travel van
+          rental. Our vans are recertified before each trip to ensure your
+          travel plans can go off without a hitch. (Hitch costs extra 😉) Our
+          team is full of vanlife enthusiasts who know firsthand the magic of
+          touring the world on 4 wheels.
+        </p>
 
-      <h3>Your destination is waiting. Your van is ready.</h3>
+        <h3>Your destination is waiting. Your van is ready.</h3>
 
-      <button className="btn-black">Explore our vans</button>
+        <button className="btn-black">Explore our vans</button>
+      </div>
       <footer>Ⓒ 2026 #VANLIFE</footer>
     </>
   );
